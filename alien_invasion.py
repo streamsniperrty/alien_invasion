98a99c8e9ed6e60ceda844d42c03ef0a8c101a50
streamsniperrty <labibmahmudeusha1@gmail.com>
2022-07-05 00:52:34 -0400

    Settings class created.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,16 @@
 # Imports
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     # Overall class to manage game assets and behavior
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -18,6 +21,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw background color during the loop
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
 
