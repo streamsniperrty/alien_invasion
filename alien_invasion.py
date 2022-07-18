0ae0ac1d28a76b2ac17e5d8d0c18271bde2ba1a2
streamsniperrty <labibmahmudeusha1@gmail.com>
2022-07-17 21:58:20 -0400

    Bullet creation and management.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     # Overall class to manage game assets and behavior
@@ -15,6 +16,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def _check_keydown_events(self, event):
         # Respond to keypresses
@@ -29,6 +31,9 @@ class AlienInvasion:
 
         elif event.key == pygame.K_q:
             sys.exit()
+
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         # Respond to key releases
@@ -51,12 +56,20 @@ class AlienInvasion:
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
 
+    def _fire_bullet(self):
+        """ Create a new bullet and add it to the bullets group """
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
         # Update images on screen, and flip to new screen
         # Redraw background color during the loop
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
+
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
+
             self.ship.update()
 
             pygame.display.flip()
@@ -66,6 +79,8 @@ class AlienInvasion:
         while True:
             # Events method
             self._check_events()
+            self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
 if __name__ == '__main__':
